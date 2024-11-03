ce46f2b33a1e89144cfce8d82bef7d25923b0cea
RafaelBD <rafa.bediaz@gmail.com>
2024-11-03 00:34:16 -0600

    improved responsive design

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -15,128 +15,75 @@ const Home = () => {
             <NavBar />
             {/* HERO */}
             <header className="contenedor">
-                <h1 className="mt-5 text-5xl z-10"
-                >
+                <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl text-white">
                     Bienvenido a Barber Shop
                 </h1>
-                <p 
-                    className="text-center text-2xl z-10 "
-                >
+                <p class="mb-6 text-lg font-normal text-white lg:text-xl sm:px-16 xl:px-48">
                     Tu lugar de confianza para un corte de cabello y afeitado
                 </p>
-                <a href="/" className="text-white text-lg p-4 border border-[#CA8A04] rounded-lg mt-4 z-10 bg-[#CA8A04] uppercase font-semibold transition ease-in-out  hover:bg-transparent duration-300 hover:border hover:border-[#CA8A04] hover:text-white">
+                <a href="/" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-[#CA8A04] rounded-lg hover:bg-[#ca8b04d2]">
                     Conoce nuestros servicios
+                    <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                    </svg>
                 </a>
-
             </header>
 
             {/* ABOUT  */}
-            <section 
-                id="about"
-                className="p-7"
-            >
-                <div className="text-gray-900 w-3/4 mx-auto p-4 m-20 grid grid-cols-2 gap-10">
-                    <div className="p-4 text-justify">
-                        <div>
-                            <h2 className="text-4xl font-semibold gold">
-                                Barber Shop
-                            </h2>
-                        </div>
-                        <div>
-                            <p className="text-gray-900 text-lg mt-4 mb-4 leading-loose">
-                                Nos destacamos por proporcionar una atención más íntima, personalizada y detallada, transformando esta tradición consagrada en una experiencia placentera y destacada para el hombre de hoy, donde cortarse el cabello, cuidarse la piel y afeitarse se convierten en una terapia de amigos y relajación, más que en un hábito.
-                            </p>
-                            <p className="text-gray-900 text-lg mt-4 mb-4 leading-loose">
-                                Nos esforzamos por brindar un santuario de cuidado personal que combine a la perfección; el encanto del pasado con las exigencias del presente.
-                            </p>
-                        </div>
-                        <div className="mt-10">
-                            <a href="/" className="text-white text-lg p-4 rounded-lg mt-4 bg-[#CA8A04] uppercase font-semibold tracking-[.2rem] transition ease-in-out hover:-translate-y-14 hover:scale-110 hover:bg-transparent duration-300 hover:border hover:border-[#CA8A04] hover:text-[#CA8A04]">
-                                conócenos
-                            </a>
-                        </div>
+            
+            <section class="bg-white">
+                <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+                    <div class="font-light sm:text-lg">
+                        <h2 class="mb-4 text-4xl tracking-tight font-bold">Barber Shop</h2>
+                        <p class="mb-4">
+                            Nos destacamos por proporcionar una atención más íntima, personalizada y detallada, transformando esta tradición consagrada en una experiencia placentera y destacada para el hombre de hoy, donde cortarse el cabello, cuidarse la piel y afeitarse se convierten en una terapia de amigos y relajación, más que en un hábito.
+                        </p>
+                        <p class="mb-4">
+                            Nos esforzamos por brindar un santuario de cuidado personal que combine a la perfección; el encanto del pasado con las exigencias del presente.
+                        </p>
                     </div>
-                    <div className="grid grid-cols-2 gap-8 h-auto">
+                    <div class="grid grid-cols-2 gap-4 mt-8">
                         {data.about.map((image, index) => (
-                            <img 
-                                src={image.src} 
-                                alt={image.alt} 
-                                className="w-full h-full object-cover" 
+                            <img
+                                src={image.src}
+                                alt={image.alt}
+                                className={`w-full rounded-lg ${index === 1 ? "mt-4 lg:mt-10" : ""}`}
                             />
                         ))}
                     </div>
                 </div>
             </section>
 
-            {/* SERVICES */}
-            <section id="about" className="p-7 bg-gray-100">
-                <div className="text-gray-900 w-3/4 mx-auto p-4 m-20 gap-10">
-                    <div className="flex flex-col gap-8 mb-16">
-                        <h2 className="text-4xl font-semibold gold text-center">
-                            Nuestros Servicios
-                        </h2>
-                        <p className="text-center text-lg mb-4 leading-loose px-36 py-0">
-                            Bienvenido a una experiencia de cuidado personal como ninguna otra. Los servicios que ofrecemos van más allá de lo común, adoptando la herencia de la barbería clásica e incorporando a la perfección técnicas de vanguardia.
-                        </p>
-                    </div>
-                    <div className="grid grid-cols-2 h-auto items-stretch">
-                        <div className="h-full">
-                            <img 
-                            src="https://images.unsplash.com/photo-1603899968034-1a56ca48d172?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-                            alt="servicio" 
-                            className="w-full h-full rounded-lg shadow-lg object-cover" 
-                            />
-                        </div>
-                        <div className="px-4 flex flex-col justify-between h-full">
-                            {data.services.map((service, index) => (
-                                <div key={index} className="flex bg-[#0000000c] items-center rounded-lg">
-                                    <div className="bg-[#CA8A04] py-16 px-6">
-                                        <span className="text-white text-5xl">{service.icon}</span>
-                                    </div>
-                                    <div className="px-10">
-                                        <h3 className="text-3xl mb-2">{service.title}</h3>
-                                        <p className="text-lg">{service.description}</p>
-                                    </div>
-                                </div>
-                            ))}
-                            
-                        </div>
-                    </div>
-                </div>
-                </section>
-
-
-
             {/* GALLERY */}
-            <section 
+            <section
                 id="gallery"
                 className="p-7"
             >
                 <div className="text-gray-900 w-3/4 mx-auto">
                     <div className="p-4">
                         <div>
-                                <h2 className="text-4xl font-semibold gold text-center">
-                                    Galería
-                                </h2>
+                            <h2 className="text-4xl font-semibold gold text-center">
+                                Galería
+                            </h2>
                         </div>
                         <div className="grid grid-cols-3 gap-4 mt-5 grid-gallery">
                             {data.gallery.map((image, index) => (
                                 <div key={index} className="relative overflow-hidden w-full h-auto">
-                                    <img 
-                                        src={image.src} 
-                                        alt={image.alt} 
-                                        className="w-full h-auto object-contain transition-transform duration-500 hover:scale-110" 
+                                    <img
+                                        src={image.src}
+                                        alt={image.alt}
+                                        className="w-full h-auto object-contain transition-transform duration-500 hover:scale-110"
                                     />
-                                </div> 
+                                </div>
                             ))}
                         </div>
                     </div>
-                    
+
                 </div>
-                
+
             </section>
 
-            <section id="location">
+            {/* <section id="location">
                 <div>
                     <iframe
                         title="mapa"
@@ -148,7 +95,7 @@ const Home = () => {
                         loading="lazy"
                     ></iframe>
                 </div>
-            </section>
+            </section> */}
             {/* {isAuthenticated ? (
                     <>
                         <BookAppointment/>
@@ -162,7 +109,7 @@ const Home = () => {
                     </button>
             )} */}
         </div>
-        
+
     )
 }
 
