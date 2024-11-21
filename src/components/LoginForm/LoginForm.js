742863f3e5f06e236375c41a83a3fd13d9cada4a
RafaelBD <rafa.bediaz@gmail.com>
2024-11-21 16:45:15 -0600

    Refactor LoginForm component to remove unused variables

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -3,7 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginForm = () => {
 
-    const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
+    const { loginWithRedirect } = useAuth0();
 
 
 
