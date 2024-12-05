a65589e8ccff8b59ecbf308f82224e99c6c73c16
RaffaPC <rafa.bediaz@gmail.com>
2024-12-05 17:24:01 -0600

    Update loading state in App component and adjust route paths for services and reservations

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import { useAuth0 } from '@auth0/auth0-react';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
-import SignUp from './pages/SignUp/SignUp';
-import BookAppointment from './pages/BookAppointment';
 import Servicios from './pages/Servicios/Services';
 import Reservations from './Client/Reservations/Reservations';
 
@@ -22,7 +20,13 @@ function App() {
   const { isLoading } = useAuth0();
 
   if (isLoading) {
-    return <div>Cargando...</div>;
+    return (
+      <button type="button" class="bg-[#eaa108] text-white" disabled>
+        <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
+        </svg>
+        Cargando...
+      </button>
+    )
   }
 
   return (
@@ -30,20 +34,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/book" element={<BookAppointment />} />
-        <Route path="/services" element={<Servicios />} />
+        <Route path="/servicios" element={<Servicios />} />
         {/* Rutas protegidas */}
         <Route
-          path="/reservations"
-          element={
-            <ProtectedRoute>
-              <Reservations />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/book"
+          path="/reservas"
           element={
             <ProtectedRoute>
               <Reservations />
